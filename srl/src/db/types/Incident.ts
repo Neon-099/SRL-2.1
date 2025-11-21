cea21f247a493bdd4e3c1b2cafc200acd8d2f7ac
Neon-099 <Emmannatividad099@gmail.com>
2025-11-21 14:41:49 +0800

    update safety type

---
+++ srl/src/db/types/Incident.ts
@@ -0,0 +1,13 @@
+import { BaseModel } from "./BaseModel"
+
+export interface Incident extends BaseModel {
+    id: string
+    type: 'incident' | 'report'
+    title: string
+    description: string
+    photos?: string[]
+    location: {lat: number, lng: number}
+    createAt: number
+    updateAt: number
+    synced: boolean
+}
