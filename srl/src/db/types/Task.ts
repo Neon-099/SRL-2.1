f5ff7fbce6af8e1c727f7a6e55626f5e19aa9ea5
Neon-099 <Emmannatividad099@gmail.com>
2025-11-28 16:40:04 +0800

    task types

--- srl/src/db/types/Task.ts
+++ srl/src/db/types/Task.ts
@@ -1,10 +1,15 @@
 import { BaseModel } from './BaseModel.ts';
 
 export interface Task extends BaseModel {
+    id: string
     title: string
     description: string
+    entity: 'task'
     status: 'Pending' | 'In Progress' | 'Completed'
     assignedTo: string 
     location: {lat: number, lng: number}
     attachments?: string[]
+    createdAt: number
+    updatedAt: number
+    synced: boolean
 }
