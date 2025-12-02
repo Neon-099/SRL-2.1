996025435f6d8e6a31561d41606275f23c4ff1a6
Neon-099 <Emmannatividad099@gmail.com>
2025-12-02 11:59:41 +0800

    footer

---
+++ srl/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = ({}) => {
+    return (
+         <div className="flex gap-6 text-sm text-gray-600">
+            <a href="#" className="hover:text-gray-800">Help Center</a>
+            <a href="#" className="hover:text-gray-800">Contact</a>
+            <a href="#" className="hover:text-gray-800">Privacy Policy</a>
+        </div>
+    )
+}
+
+export default Footer;
