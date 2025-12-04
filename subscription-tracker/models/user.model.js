60ea96300face93594c1f9928c08c943fca74039
Neon-099 <Emmannatividad099@gmail.com>
2025-12-04 21:23:18 +0800

    models

---
+++ subscription-tracker/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, 'User Name is required'],
+        trim: true,
+        minLength: 2,
+        maxLength: 50
+    },
+    email: {
+        type: String, 
+        required: [true, 'Email is required'],
+        unique: true, //SINCE 1 USER ONLY HAS THAT EMAIL
+        trim: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: 6,
+    },
+
+}, {timestamps: true})
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
